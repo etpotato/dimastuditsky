87db5413639b07a4a273813722744fdd2eef006d
Pavel Mareev <mareev.pv@gmail.com>
2025-10-05 19:14:04 +0200

    feat: remove tooltip

--- app/components/Sets/components/HoverTooltip/HoverTooltip.utils.ts
+++
@@ -1,6 +0,0 @@
-export const sources = {
-    'soundcloud': 'Soundcloud',
-    'youtube': 'Youtube',
-    'spotify': 'Spotify',
-    'yandex': 'Yandex Music' 
-} as const;
--- app/components/Sets/components/SetCard/SetCard.tsx
+++ app/components/Sets/components/SetCard/SetCard.tsx
@@ -1,52 +1,48 @@
 import type { Track } from "~/types";
-import styles from "./SetCard.module.css"
+import styles from "./SetCard.module.css";
 import { Link, Text } from "~/ui";
 import { getSource } from "../../utils/getSource";
 import { CardIcon } from "../CardIcon/CardIcon";
 import { getFormattedDuration } from "../../utils/getFormattedDuration";
-import { HoverTooltip } from "../HoverTooltip/HoverTooltip";
 import { memo } from "react";
 
 interface Props {
-    card: Track;
-    isGroup?: boolean;
-    index?: number;
+  card: Track;
+  isGroup?: boolean;
+  index?: number;
 }
 
-export const SetCard = memo(function ({card, isGroup, index}: Props) {
-    const {created_at, artwork_url, duration, title, url} = card;
-    const source = getSource(url)
-    const formattedDuration = getFormattedDuration(duration)
-    const trackDate = new Date(created_at)
-    const date = `${('0' + trackDate.getDate()).slice(-2)}.${('0' + (trackDate.getMonth() + 1)).slice(-2) }`
+export const SetCard = memo(function ({ card, isGroup, index }: Props) {
+  const { created_at, artwork_url, duration, title, url } = card;
+  const source = getSource(url);
+  const formattedDuration = getFormattedDuration(duration);
+  const trackDate = new Date(created_at);
+  const date = `${("0" + trackDate.getDate()).slice(-2)}.${("0" + (trackDate.getMonth() + 1)).slice(-2)}`;
 
-    
-    return(
-        <div className={styles.setCard}>
-            <Text content={date} className={styles.setDate}/>
-            <HoverTooltip url={url} className={styles.setTooltip}>
-                <Link
-                    href={url}
-                >
-                    <div className={styles.setCell}>
-                        {isGroup 
-                            ? index === 0 && <Text content={date} className={styles.setDatePc}/> 
-                            : <Text content={date} className={styles.setDatePc}/>}
-                        <div className={styles.setImgs}>
-                            <img 
-                                src={artwork_url} 
-                                className={styles.setImg}
-                                alt={`Artwork ${title}`} 
-                            />
-                            <div className={styles.setSource}>
-                                <CardIcon source={source} />
-                            </div>
-                        </div>
-                        <Text content={title.toUpperCase()} className={styles.setTitle}/>
-                    </div>
-                </Link>
-            </HoverTooltip>
-            <Text content={formattedDuration} className={styles.setDuration}/>
+  return (
+    <div className={styles.setCard}>
+      <Text content={date} className={styles.setDate} />
+      <Link href={url}>
+        <div className={styles.setCell}>
+          {isGroup ? (
+            index === 0 && <Text content={date} className={styles.setDatePc} />
+          ) : (
+            <Text content={date} className={styles.setDatePc} />
+          )}
+          <div className={styles.setImgs}>
+            <img
+              src={artwork_url}
+              className={styles.setImg}
+              alt={`Artwork ${title}`}
+            />
+            <div className={styles.setSource}>
+              <CardIcon source={source} />
+            </div>
+          </div>
+          <Text content={title.toUpperCase()} className={styles.setTitle} />
         </div>
-    )
-})
+      </Link>
+      <Text content={formattedDuration} className={styles.setDuration} />
+    </div>
+  );
+});
